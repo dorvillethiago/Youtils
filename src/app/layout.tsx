47d77679a741bb179dc2a0158563090b125a2fd9
dorvillethiago <dorville.dev@gmail.com>
2023-10-14 12:58:40 -0300

    feat: novo componente de header e melhorias no código do layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import '@/src/styles/globals.scss'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Header from '../components/Header/Header'
+import styles from "./layout.module.scss"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Youtils',
-  description: 'Youtube opensource utility tools',
+  description: 'Youtube open-source utility tools',
 }
 
 export default function RootLayout({
@@ -16,7 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div style={styles} className={styles.container}>
+          <Header/>
+          <main>
+            {children}
+          </main>
+        </div>
+      </body>
     </html>
   )
 }
