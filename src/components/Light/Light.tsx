df68ae841f899d1965507550d594661fdff4a5c8
dorvillethiago <dorville.dev@gmail.com>
2023-10-21 22:12:17 -0300

    feat: adicionada luz dinâmica que segue o mouse

--- src/components/Light/Light.tsx
+++ src/components/Light/Light.tsx
@@ -1,9 +1,10 @@
 import styles from "./Light.module.scss";
+import MouseFollower from "./MouseFollower";
 
 export default function Light() {
   return (
     <div className={styles.container}>
-      <div className={styles.light} />
+      <MouseFollower/>
     </div>
   );
 }
