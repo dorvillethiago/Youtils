f6761ce7efe6d10370d0bb3b7433f4ea24b6705d
Denison-Oliveira <juniod04@gmail.com>
2023-10-31 16:43:53 -0300

    fix:resolvendo problema na tela de download

--- src/app/video/[id]/page.tsx
+++ src/app/video/[id]/page.tsx
@@ -1,10 +1,20 @@
+import Thubnail from "@components/Download/Thumbnail";
+import Title from "@components/Download/Title";
+import Timer from "@components/Download/Timer";
+import Button from "@components/Download/Button";
 
+import styles from "@styles/components/Download/DownloadPage.module.scss"
 
 
 export default function Page () {
     return(
-        <>
-            oi
-        </>
+        <div className={styles.container}>
+            <Thubnail src={"/thumbnail.svg"}/>
+            <div className={styles.organizeThumbnail}>
+                <Title/>
+                <Timer/>
+                <Button format="mp3"/>
+            </div>
+        </div>
     );
 }
