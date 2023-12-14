28bd59520d7c42cff5aa91b19e31e71471e9f817
dorvillethiago <dorville.dev@gmail.com>
2023-12-14 16:04:23 -0300

    chore: added youtube thumbnail domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const path = require('path')
  
 module.exports = {
+  images: {
+    domains: ['i.ytimg.com'],
+  },
+  reactStrictMode: false,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
